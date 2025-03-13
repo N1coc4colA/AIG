f2d0be25237ec002f748e2b76ed1c15e507d2050
Nicolas Badin <50690360+N1coc4colA@users.noreply.github.com>
2025-03-13 23:52:32 +0100

    Implement backend services

--- backend/src/main/java/com/aig/backend/services/Chat.java
+++ backend/src/main/java/com/aig/backend/services/Chat.java
@@ -1,4 +1,33 @@
 package com.aig.backend.services;
 
-public class Chat {
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.openai.OpenAiChatOptions;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Chat implements IChatService {
+    private final ChatModel chatModel;
+
+    public Chat(ChatModel chatModel) {
+        this.chatModel = chatModel;
+    }
+
+    public String getResponse(String prompt) {
+        return chatModel.call(prompt);
+    }
+
+    public String getResponseOptions(String prompt) {
+        final ChatResponse response = chatModel.call(
+                new Prompt(
+                        prompt,
+                        OpenAiChatOptions.builder()
+                                .withModel("gpt-4o")
+                                .withTemperature(0.4F)
+                                .build()
+                ));
+
+        return response.getResult().getOutput().getContent();
+    }
 }
--- backend/src/main/java/com/aig/backend/services/Image.java
+++ backend/src/main/java/com/aig/backend/services/Image.java
@@ -1,4 +1,30 @@
 package com.aig.backend.services;
 
-public class Image {
+import org.springframework.ai.image.ImagePrompt;
+import org.springframework.ai.image.ImageResponse;
+import org.springframework.ai.openai.OpenAiImageModel;
+import org.springframework.ai.openai.OpenAiImageOptions;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Image implements IImageService {
+    private final OpenAiImageModel openAiImageModel;
+
+    public Image(OpenAiImageModel openAiImageModel) {
+        this.openAiImageModel = openAiImageModel;
+    }
+
+    public ImageResponse generateImage(String prompt, String quality, int n, int width, int height) {
+        final ImageResponse imageResponse = openAiImageModel.call(
+                new ImagePrompt(prompt,
+                        OpenAiImageOptions.builder()
+                                .withModel("dall-e-2")
+                                .withQuality(quality)
+                                .withN(n)
+                                .withHeight(height)
+                                .withWidth(width).build())
+        );
+
+        return imageResponse;
+    }
 }
