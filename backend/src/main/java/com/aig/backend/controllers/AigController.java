47cb3d717bb304fc137812130fb4b491da23b5fe
Nicolas Badin <50690360+N1coc4colA@users.noreply.github.com>
2025-03-13 23:53:41 +0100

    Implement backend controller

--- backend/src/main/java/com/aig/backend/controllers/AigController.java
+++ backend/src/main/java/com/aig/backend/controllers/AigController.java
@@ -1,4 +1,60 @@
 package com.aig.backend.controllers;
 
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.ai.image.ImageResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import com.aig.backend.services.IChatService;
+import com.aig.backend.services.IImageService;
+import com.aig.backend.services.IRecipeService;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
 public class AigController {
+    private final IChatService chatService;
+    private final IImageService imageService;
+    private final IRecipeService recipeService;
+
+    public AigController(IChatService chatService, IImageService imageService, IRecipeService recipeService) {
+        this.chatService = chatService;
+        this.imageService = imageService;
+        this.recipeService = recipeService;
+    }
+
+    @GetMapping("chat")
+    public String getResponse(@RequestParam String prompt) {
+        return chatService.getResponse(prompt);
+    }
+
+    @GetMapping("chat-options")
+    public String getResponseOptions(@RequestParam String prompt) {
+        return chatService.getResponseOptions(prompt);
+    }
+
+    @GetMapping("image")
+    public List<String> generateImages(HttpServletResponse response,
+                                       @RequestParam String prompt,
+                                       @RequestParam(defaultValue = "hd") String quality,
+                                       @RequestParam(defaultValue = "1") int n,
+                                       @RequestParam(defaultValue = "1024") int width,
+                                       @RequestParam(defaultValue = "1024") int height) throws IOException {
+        ImageResponse imageResponse = imageService.generateImage(prompt, quality, n, width, height);
+
+        // Streams to get urls from ImageResponse
+        List<String> imageUrls = imageResponse.getResults().stream()
+                .map(result -> result.getOutput().getUrl())
+                .toList();
+
+        return imageUrls;
+    }
+
+    @GetMapping("recipe")
+    public String recipeCreator(@RequestParam String ingredients,
+                                @RequestParam(defaultValue = "any") String cuisine,
+                                @RequestParam(defaultValue = "") String dietaryRestriction) {
+        return recipeService.createRecipe(ingredients, cuisine, dietaryRestriction);
+    }
 }
