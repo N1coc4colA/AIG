926397ebb0ba82843b056a453ffd49eda9ac7498
Nicolas Badin <50690360+N1coc4colA@users.noreply.github.com>
2025-03-15 22:49:16 +0100

    Add queries from frontend to backend

---
+++ frontend/scripts/main.js
@@ -0,0 +1,112 @@
+const messages = document.getElementById("messagingFlex");
+const imaging = document.getElementById("imagingFlex");
+const chatPromptField = document.getElementById("chat-inputArea");
+const imgPromptField = document.getElementById("img-inputArea");
+const ingredientsField = document.getElementById("rcp-ing-inputArea");
+const restrictionsField = document.getElementById("rcp-not-inputArea");
+const cuisineField = document.getElementById("rcp-type-inputArea");
+const recipeArea = document.getElementById("recipeArea");
+const converter = new showdown.Converter();
+let img_gen_counter = 0;
+
+function submitChat()
+{
+    const prompt = chatPromptField.value;
+
+    const msg = document.createElement("div");
+    msg.classList.add("message", "user");
+    msg.innerText = prompt;
+    messages.appendChild(msg);
+
+    const params = new URLSearchParams({
+        prompt: prompt,
+    });
+
+    return fetch("http://localhost:3306/chat?" + params.toString())
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        return response.data().toString();
+    })
+    .then(data => {
+        const assMsg = document.createElement("div");
+        assMsg.classList.add("message", "assistant");
+        assMsg.innerText = data;
+
+        return true;
+    })
+    .catch(error => {
+        console.error("Error in request:", error);
+        return false;
+    });
+}
+
+function generateImage()
+{
+    const prompt = imgPromptField.value;
+    const params = new URLSearchParams({
+        prompt: prompt,
+    });
+
+    return fetch("http://localhost:3306/image?" + params.toString())
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        return response.json();
+    })
+    .then(data => {
+        data.forEach(url => {
+            const container = document.createElement("div");
+            container.classList.add("col-10", "col-md-5", "col-xl", "img-gen", "p-0", "", "");
+
+            const img = document.createElement("img");
+            img.classList.add("img-fluid", "roundit");
+            img.src = url;
+            img.alt = "AI-Generated Image";
+            img.id = "img-gen-" + img_gen_counter.toString();
+
+            container.appendChild(img);
+            imaging.appendChild(container);
+            img_gen_counter++;
+        });
+
+        return true;
+    })
+    .catch(error => {
+        console.error("Error in request:", error);
+        return false;
+    });
+}
+
+function generateRecipe()
+{
+    const ingredients = ingredientsField.value;
+    const restrictions = restrictionsField.value;
+    const cuisine = cuisineField.value;
+    const params = new URLSearchParams({
+        cuisine: cuisine,
+        ingredients: ingredients,
+        dietaryRestriction: restrictions,
+    });
+
+    return fetch("http://localhost:3306/recipe?" + params.toString())
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        return response.data().toString();
+    })
+    .then(data => {
+        recipeArea.innerHTML = converter.makeHtml(data);
+        return true;
+    })
+    .catch(error => {
+        console.error("Error in request:", error);
+        return false;
+    });
+}
